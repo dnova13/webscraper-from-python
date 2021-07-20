a61014207d927cd162f2d05995e8cbe45e95606b
dnova13 <dnova13@gmail.com>
2021-07-21 04:10:30 +0900

    Extracting Companies

--- indeed.py
+++ indeed.py
@@ -44,19 +44,32 @@ def extract_indee_jobs(last_page) :
 
         ### 각 페이지의 각 태그 <div class='jobsearch-serpJobCart'> 에서 직업들 추출. 
         ### div 태그에서 클래스는 jobsearch-serpJobCart 인 텍스트 추출  
+        ### find_all : 검색하는 키워드 전부 찾음
+        ### find : 검색하는 키워드 중 하나만 찾음
         results = soup.find_all("div", class_="job_seen_beacon")
         
         for result in results :
-            title = result.find("h2", {"class":"jobTitle"})
-            # print(title);
-            
+
+            ### 직업 추출
             ### span 태그 속성 검색 하면 <sapn title='직업 제목'> 이고  class 는 없으므로
             ### span 속성이 title 인거와 class과 없는거 추출 
             ### 하지만 title 에 title 속성 값이 없는게 잇어서 추출 안되므로 코드 변경
-            span = title.find('span', {'class':None})["title"];
-            print(span)
+            title = result.find("h2", {"class":"jobTitle"}).find('span', {'class':None})["title"];
             
-            jobs.append(span)
+            ### 회사 이름 추출
+            company_span = result.find("span", class_="companyName");
+            company_anchor = company_span.find('a')
+
+            if company_anchor is not None :
+                company = str(company_anchor.string) ### string 으로 변경.
+                # print(company_anchor.string)
+            else :
+                company = str(company_span.string)
+                # print(company_span.string)
+            
+            company = company.strip(); ### 빈 공간을 없앰. 다른 예로 strip("F") F로 된 글자 다 없애줌.
+
+            print(title, company)
         
     return jobs;
 
